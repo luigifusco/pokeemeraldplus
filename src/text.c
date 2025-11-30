bcdd1626ddc02b5469b0f222447df59675943f94
Luigi Fusco <luigifusco98@gmail.com>
2025-12-01 00:12:44 +0100

    Instant text

--- src/text.c
+++ src/text.c
@@ -326,11 +326,21 @@ void RunTextPrinters(void)
         {
             if (sTextPrinters[i].active)
             {
-                u16 renderCmd = RenderFont(&sTextPrinters[i]);
+                u16 renderCmd;
+
+                do
+                {
+                    renderCmd = RenderFont(&sTextPrinters[i]);
+                    if (renderCmd == RENDER_PRINT)
+                    {
+                        CopyWindowToVram(sTextPrinters[i].printerTemplate.windowId, COPYWIN_GFX);
+                        if (sTextPrinters[i].callback != NULL)
+                            sTextPrinters[i].callback(&sTextPrinters[i].printerTemplate, renderCmd);
+                    }
+                } while (renderCmd == RENDER_PRINT);
+
                 switch (renderCmd)
                 {
-                case RENDER_PRINT:
-                    CopyWindowToVram(sTextPrinters[i].printerTemplate.windowId, COPYWIN_GFX);
                 case RENDER_UPDATE:
                     if (sTextPrinters[i].callback != NULL)
                         sTextPrinters[i].callback(&sTextPrinters[i].printerTemplate, renderCmd);
